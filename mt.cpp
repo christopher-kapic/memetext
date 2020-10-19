aea418048fefb7255d6da4b05aa050403c4808f7
Christopher Kapic <59740769+christopher-kapic@users.noreply.github.com>
2020-10-18 22:34:53 -0500

    Wrote on PC, going to compile on mac.

--- mt.cpp
+++ mt.cpp
@@ -1,16 +1,40 @@
-#include <iostream>
+// #include <iostream>
 
-char *finalString(char *s)
-{
+// char *finalString(char *s)
+// {
+// }
+
+// int main(int argc, char *argv[])
+// {
+//     if (!argv[1])
+//     {
+//         fprintf(stderr, "Please provide text for\n");
+//         return 1;
+//     }
+
+//     return 0;
+// }
+
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+char* memeText(char* text){
+    char* text_ = text;
+    for (; *text_ != "\0"; text_++) {
+        if (rand() % 2 == 0){
+            *text_ = "e";
+        } else {
+            *text_ = "i";
+        }
+    }
+    return (*text_);
 }
 
-int main(int argc, char *argv[])
-{
-    if (!argv[1])
-    {
-        fprintf(stderr, "Please provide text for\n");
-        return 1;
+int main(int argc, char *argv[]){
+    if (!argv[1]){
+        std::cerr << "Please provide an argument for memetext.\n";
     }
 
-    return 0;
+    return memeText(argv[1]);
 }
